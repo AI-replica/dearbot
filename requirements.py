a0ede8984d69be9b676c679c6cc266427f708c4d
Roman Sitelew <19929162+RomanPlusPlus@users.noreply.github.com>
2024-12-01 17:14:40 +0100

    📌build(dep): pin dependencies

--- requirements.py
+++ requirements.py
@@ -1,7 +1,7 @@
-anthropic
-Pillow
-dearpygui
-pygments
-mistletoe
-numpy
-tiktoken
+anthropic==0.40.0
+Pillow==11.0.0
+dearpygui==2.0.0
+pygments==2.18.0
+mistletoe==1.4.0
+numpy==2.1.3
+tiktoken==0.8.0
